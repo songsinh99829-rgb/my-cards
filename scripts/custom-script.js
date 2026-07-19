5b72f8f4d3ea295814348b6282a4f105580d16d0
songsinh99829-rgb <songsinh99829@gmail.com>
2026-07-19 10:17:10 +0700

    Update translated external link

---
+++ scripts/custom-script.js
@@ -0,0 +1,140 @@
+{
+  "Khớp Chính Xác": {
+    "Rasputin": [
+      "https://cdn.imgchest.com/files/f6331bee5594.jpg",
+      "https://cdn.imgchest.com/files/ee81c1f5c7e1.jpg"
+    ],
+    "Anna": [
+      "https://cdn.imgchest.com/files/255aca413517.jpg"
+    ],
+    "Yekaterina Đệ Nhất": [
+      "https://cdn.imgchest.com/files/488d6cfdf8ff.jpg",
+      "https://cdn.imgchest.com/files/8bf44009fb27.jpg"
+    ],
+    "Marta": [
+      "https://cdn.imgchest.com/files/488d6cfdf8ff.jpg",
+      "https://cdn.imgchest.com/files/8bf44009fb27.jpg"
+    ],
+    "Yekaterina II": [
+      "https://cdn.imgchest.com/files/8ecb99b7725b.jpg",
+      "https://cdn.imgchest.com/files/7bba245d90b1.jpg"
+    ],
+    "Sophia": [
+      "https://cdn.imgchest.com/files/8ecb99b7725b.jpg",
+      "https://cdn.imgchest.com/files/7bba245d90b1.jpg"
+    ],
+    "Anastasia": [
+      "https://cdn.imgchest.com/files/d9052bd7022b.jpg",
+      "https://cdn.imgchest.com/files/c829d42e1df9.jpg"
+    ],
+    "Sophia": [
+      "https://cdn.imgchest.com/files/1de52098ac43.jpg",
+      "https://cdn.imgchest.com/files/2802f7deb4c9.jpg"
+    ],
+    "Annie": [
+      "https://cdn.imgchest.com/files/6a82a6187c28.jpg",
+      "https://cdn.imgchest.com/files/a1dba87eac18.jpg"
+    ],
+    "Yelena": [
+      "https://cdn.imgchest.com/files/416570c8066d.jpg"
+    ],
+    "Maria": [
+      "https://cdn.imgchest.com/files/0c9f030d9f4c.jpg"
+    ],
+    "Trần Yến": [
+      "https://cdn.imgchest.com/files/6ef204a594a1.jpg",
+      "https://cdn.imgchest.com/files/cf638a074a6c.jpg"
+    ],
+    "Antonina": [
+      "https://cdn.imgchest.com/files/01568ada3dc0.jpg"
+    ],
+    "Renata": [
+      "https://cdn.imgchest.com/files/c16fd36ce295.jpg",
+      "https://cdn.imgchest.com/files/d6e68e3f5b31.jpg"
+    ],
+    "Zero": [
+      "https://cdn.imgchest.com/files/0d86c562c3e8.jpg"
+    ],
+    "Svetlana": [
+      "https://cdn.imgchest.com/files/814e687b98e6.jpg"
+    ],
+    "Vera": [
+      "https://cdn.imgchest.com/files/f31836a0439d.jpg",
+      "https://cdn.imgchest.com/files/7777525cf570.jpg"
+    ]
+  },
+
+  "Nhỏ, tóc hai chùm thấp": 
+  [],
+  "Vừa, tóc hai chùm thấp":  
+  [],
+  "Lớn, tóc hai chùm thấp": 
+  [],
+
+  "Nhỏ, tóc búi":
+  [],
+  "Vừa, tóc búi": 
+  [],
+  "Lớn, tóc búi": 
+  [],
+
+  "Nhỏ, tóc thắt bím": 
+  [],
+  "Vừa, tóc thắt bím": 
+  [],
+  "Lớn, tóc thắt bím": 
+  [],
+
+  "Nhỏ, tóc buộc thấp":  
+  [],
+  "Vừa, tóc buộc thấp": 
+  [],
+  "Lớn, tóc buộc thấp":
+  [],
+
+  "Nhỏ, tóc xoăn": 
+  [],
+  "Vừa, tóc xoăn":  
+  [],
+  "Lớn, tóc xoăn": 
+  [],
+
+  "Nhỏ, tóc buộc lệch": 
+  [],
+  "Vừa, tóc buộc lệch": 
+  [],
+  "Lớn, tóc buộc lệch": 
+  [],
+
+  "Nhỏ, tóc hai chùm":
+  [],
+  "Vừa, tóc hai chùm": 
+  [],
+  "Lớn, tóc hai chùm": 
+  [],
+
+  "Nhỏ, tóc dài": 
+  [],
+  "Vừa, tóc dài": 
+  [],
+  "Lớn, tóc dài": 
+  [],
+
+  "Nhỏ, tóc ngắn": 
+  [],
+  "Vừa, tóc ngắn":  
+  [],
+  "Lớn, tóc ngắn": 
+  [],
+  
+  "Nhỏ, tóc đuôi ngựa": 
+  [],
+  "Vừa, tóc đuôi ngựa": 
+  [],
+  "Lớn, tóc đuôi ngựa": 
+  [],
+
+  "Không khớp": [
+    "https://cdn.imgchest.com/files/887cd59fdacb.jpg"
+  ]
+}
